ecf180020546fc26297881351c7ea21895902a93
naiya <naiyajani10@gmail.com>
2024-03-31 22:57:53 -0400

    Fix: Store Data file proper commented

--- src/main/java/org/clothing/scraper/StoreDataInFile.java
+++ src/main/java/org/clothing/scraper/StoreDataInFile.java
@@ -1,6 +1,5 @@
 package org.clothing.scraper;
 
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -10,11 +9,13 @@ import java.nio.file.Paths;
 
 public class StoreDataInFile {
 
+    // Instance variables to store product information
     private String productName;
     private String productPrice;
     private String productBrand;
     private String productCategory;
 
+    // Constructors used in initializing product information
     StoreDataInFile(String pName, String pPrice, String pBrand, String pCategory) {
         productName = pName;
         productPrice = pPrice;
@@ -22,13 +23,18 @@ public class StoreDataInFile {
         productCategory = pCategory;
     }
 
+    // Static method to get the file path for a given file name
     public static String getFilePath(String fileName) {
+        // Getting  present directories
         Path currentPath = Paths.get(System.getProperty("user.dir"));
+        // Creating the way for a directory where files will be stored
         Path dirpath = Paths.get(currentPath.toString(), "assets");
 
+        // Concatenate the directory way and file name to get the complete file path
         return dirpath + "/" + fileName;
     }
 
+    // Static method to delete the file if it exists
     public static void deleteIfFileExist(String filePath) {
         File file = new File(filePath);
         if (file.exists()) {
@@ -36,46 +42,12 @@ public class StoreDataInFile {
         }
     }
 
+    // Static method to validate input (accepts only alphabets and spaces)
     public static boolean isValidInput(String input) {
         return input.matches("[a-zA-Z ]+");
     }
 
-    // Getter and Setter for productName
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    // Getter and Setter for productPrice
-    public String getProductPrice() {
-        return productPrice;
-    }
-
-    public void setProductPrice(String productPrice) {
-        this.productPrice = productPrice;
-    }
-
-    // Getter and Setter for productBrand
-    public String getProductBrand() {
-        return productBrand;
-    }
-
-    public void setProductBrand(String productBrand) {
-        this.productBrand = productBrand;
-    }
-
-    // Getter and Setter for productCategory
-    public String getProductCategory() {
-        return productCategory;
-    }
-
-    public void setProductCategory(String productCategory) {
-        this.productCategory = productCategory;
-    }
-
+    // Method to format product data in CSV format
     public String getDataInCsvFormat() {
         return productName.replace(",", "") + " , " +
                 productPrice.replace(",", "") + " , " +
@@ -83,9 +55,10 @@ public class StoreDataInFile {
                 productCategory.replace(",", "") + "\n";
     }
 
+    // functions used to save product in given CSV file
     public void saveDataToCsv(String filePath) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
-            // Write the content to the file
+            // Write the formatted data to the file
             writer.write(this.getDataInCsvFormat());
         } catch (IOException e) {
             System.err.println("Error writing to the file: " + e.getMessage());
